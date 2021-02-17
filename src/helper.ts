2b4da445b3de78fb7c0fd1e74f1e9f9621384c53
Raymond Ng <rayng86@gmail.com>
2021-02-17 17:48:04 -0500

    add helper file
    helper for generating random rgba values, randomizing elements in an array, and fixing txt string values returned from the trivia api.

---
+++ src/helper.ts
@@ -0,0 +1,23 @@
+export const fixTextStr = (str: string) => {
+  const mapObj: any = {
+    '&#039;': '\'',
+    '&quot;': '"',
+    '&amp;': '&',
+  };
+  const re = new RegExp(Object.keys(mapObj).join("|"), "gi");
+  return str.replace(re, (matched) => mapObj[matched.toLowerCase()]);
+};
+
+export const randomizeAnswers = (arr: Array<string>) => {
+  const randomize = (a: string, b: string) => 0.5 - Math.random();
+  return arr.sort(randomize);
+};
+
+export const randomBg = () => {
+  const r = Math.floor(Math.random() * 252);
+  const g = Math.floor(Math.random() * 213);
+  const b = Math.floor(Math.random() * 206);
+  const a = 0.7;
+  const color = `rgba(${r},${g},${b},${a})`;
+  return color;
+};
